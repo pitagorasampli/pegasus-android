79a61a84cae5813dae88986d5a27259c5a1115fe
shidobecker <juliowebdeveloper@hotmail.com>
2023-06-07 18:38:43 -0300

    Added gradle.properties

--- buildSrc/src/main/kotlin/PegasusDesignSystem.kt
+++ buildSrc/src/main/kotlin/PegasusDesignSystem.kt
@@ -2,4 +2,7 @@ object PegasusDesignSystem {
     private const val designSystemVersion = "0.0.13"
 
    // const val designSystem = "com.github.pitagorasampli:ampli-design-system:$designSystemVersion"
+
+    const val designSystem =  "com.github.pitagorasampli:pegasus-design-system-android:master-SNAPSHOT"
+
 }
